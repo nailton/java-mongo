f9f81ebb175f54150ac982052f814b80119606dd
Nailton Sousa <nailton.osn@gmail.com>
2023-05-21 12:50:57 -0300

    Initial commit

---
+++ src/test/java/br/com/nailton/javamongo/JavaMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nailton.javamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
