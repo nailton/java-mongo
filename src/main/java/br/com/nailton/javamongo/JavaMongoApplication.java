ee965f0a2953678b9a6340ca6cd6322a619b7781
Nailton Sousa <nailton.osn@gmail.com>
2023-05-21 13:12:47 -0300

    Entitty User e Rest funcionando

--- src/main/java/br/com/nailton/javamongo/JavaMongoApplication.java
+++ src/main/java/br/com/nailton/javamongo/JavaMongoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaMongoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaMongoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMongoApplication.class, args);
+    }
 
 }
