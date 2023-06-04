fd06c102e9836b3b77e1ba0a26b83908e7d9fe41
Nailton Sousa <nailton.osn@gmail.com>
2023-06-03 23:04:20 -0300

    Acresentando comentário aos posts

--- src/main/java/br/com/nailton/javamongo/config/Instantiation.java
+++ src/main/java/br/com/nailton/javamongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package br.com.nailton.javamongo.config;
 import br.com.nailton.javamongo.domain.Post;
 import br.com.nailton.javamongo.domain.User;
 import br.com.nailton.javamongo.dto.AuthorDTO;
+import br.com.nailton.javamongo.dto.CommentDTO;
 import br.com.nailton.javamongo.repository.PostRepository;
 import br.com.nailton.javamongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +36,14 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c2));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
         maria.getPosts().addAll(Arrays.asList(post1, post2));
--- src/main/java/br/com/nailton/javamongo/domain/Post.java
+++ src/main/java/br/com/nailton/javamongo/domain/Post.java
@@ -1,11 +1,14 @@
 package br.com.nailton.javamongo.domain;
 
 import br.com.nailton.javamongo.dto.AuthorDTO;
+import br.com.nailton.javamongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Document
 public class Post implements Serializable {
@@ -17,6 +20,7 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
 
     public Post() {
@@ -72,6 +76,14 @@ public class Post implements Serializable {
         this.author = author;
     }
 
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -84,5 +96,6 @@ public class Post implements Serializable {
     public int hashCode() {
         return getId() != null ? getId().hashCode() : 0;
     }
+
 
 }
