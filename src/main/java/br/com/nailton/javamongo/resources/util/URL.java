1c91f1a34da64c8239359203f737d924d606f780
Nailton Sousa <nailton.osn@gmail.com>
2023-06-03 23:55:09 -0300

    Consulta com vários critérios

--- src/main/java/br/com/nailton/javamongo/resources/util/URL.java
+++ src/main/java/br/com/nailton/javamongo/resources/util/URL.java
@@ -2,6 +2,11 @@ package br.com.nailton.javamongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
     public static String decodeParam(String text) {
@@ -9,6 +14,16 @@ public class URL {
             return URLDecoder.decode(text, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             return "";
+        }
+    }
+
+    public static Date convertDate(String textDate, Date defaultValue) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        try {
+            return sdf.parse(textDate);
+        } catch (ParseException e) {
+            return defaultValue;
         }
     }
 }
--- src/main/java/br/com/nailton/javamongo/services/PostService.java
+++ src/main/java/br/com/nailton/javamongo/services/PostService.java
@@ -6,6 +6,7 @@ import br.com.nailton.javamongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -23,5 +24,10 @@ public class PostService {
 
     public List<Post> findByTitle(String text) {
         return repo.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
     }
 }
