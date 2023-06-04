0c887de3e82b80a61fcce89c846a01dedb18edb9
Nailton Sousa <nailton.osn@gmail.com>
2023-06-03 23:28:09 -0300

    Consulta simples com @Query

--- src/main/java/br/com/nailton/javamongo/repository/PostRepository.java
+++ src/main/java/br/com/nailton/javamongo/repository/PostRepository.java
@@ -3,11 +3,15 @@ package br.com.nailton.javamongo.repository;
 import br.com.nailton.javamongo.domain.Post;
 import br.com.nailton.javamongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{'title':  {$regex: ?0, $options: 'i'}}")
+    List<Post> searchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
